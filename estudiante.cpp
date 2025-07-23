0003bb20a4fe2efd02344a904e6309fbdbc4c2db
Melrh18 <130946451+Melrh18@users.noreply.github.com>
2025-07-22 22:06:35 -0600

    obtener_estudiante

--- estudiante.cpp
+++ estudiante.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <string>
 #include <tuple>
-#include <stdexcept> //manejo de exepciones 
-#include <cctype> //manejo de tipo de carácteres 
+#include <stdexcept> //manejo de exepciones
+#include <cctype>    //manejo de tipo de carácteres
 #include <vector>
-#include <sstream> //Para convertir un string en un flujo de datos tipo entrada 
-#include <fstream> //manejo de archivos 
+#include <sstream> //Para convertir un string en un flujo de datos tipo entrada
+#include <fstream> //manejo de archivos
 using namespace std;
 
 //----------------------------------------------------------------------------------------------------------------------
-//Creación de Archivo
+// Creación de Archivo
 //-----------------------------------------------------------------------------------------------------------------------
 
 ofstream archivo("estudiantes.txt", ios::app);
@@ -32,26 +32,34 @@ private:
   int edad;
   Genero genero;
 
-  string generoToString(Genero _genero) const {
+  string generoToString(Genero _genero) const
+  {
     switch (_genero)
     {
-    case Genero::Femenino: return "Femenino";
+    case Genero::Femenino:
+      return "Femenino";
       break;
-    case Genero::Masculino: return "Masculino";
+    case Genero::Masculino:
+      return "Masculino";
       break;
-    case Genero::Otro: return "Otro";
+    case Genero::Otro:
+      return "Otro";
       break;
-    
+
     default:
       throw invalid_argument("Género inválido");
       break;
     }
   }
 
-  Genero intToGenero(int _genero) const {  //Entero a Genero
-    if (_genero == 1) return Genero::Femenino;
-    if (_genero == 2) return Genero::Masculino;
-    if (_genero == 3) return Genero::Otro;
+  Genero intToGenero(int _genero) const
+  { // Entero a Genero
+    if (_genero == 1)
+      return Genero::Femenino;
+    if (_genero == 2)
+      return Genero::Masculino;
+    if (_genero == 3)
+      return Genero::Otro;
     throw invalid_argument("Género inválido");
   }
 
@@ -68,12 +76,12 @@ public:
   //---------------------------------------------------------------------------------------------------------------------------------
   // Getters and Setters
   //---------------------------------------------------------------------------------------------------------------------------------
-  int get_id() const 
+  int get_id() const
   {
     return id;
   }
 
-  string get_nombre() const 
+  string get_nombre() const
   {
     return nombre;
   }
@@ -103,32 +111,37 @@ public:
     return get<2>(residencia);
   }
 
-
   // TODO: Hacer validación de ID único haciendo un for y buscando el ID digitado con los del txt
-  void set_id(int _id) {
-    string string_id= to_string(_id);
-    if (string_id.length() != 10){
-        cout <<"La identificación debe contener 10 números" <<endl;
+  void set_id(int _id)
+  {
+    string string_id = to_string(_id);
+    if (string_id.length() != 10)
+    {
+      cout << "La identificación debe contener 10 números" << endl;
     }
-    for (char cada_numero : string_id){
-        if (!isdigit(cada_numero)){
-            cout<<"Error: La identificación no debe contener letras" <<endl;
-        }
+    for (char cada_numero : string_id)
+    {
+      if (!isdigit(cada_numero))
+      {
+        cout << "Error: La identificación no debe contener letras" << endl;
+      }
     }
     id = _id;
   }
 
   void set_nombre(string _nombre)
   {
-    vector<string> nombre_vector; 
+    vector<string> nombre_vector;
     stringstream ss(_nombre);
-    string cada_palabra; 
-    while (ss >> cada_palabra){
-        nombre_vector.push_back(cada_palabra);
+    string cada_palabra;
+    while (ss >> cada_palabra)
+    {
+      nombre_vector.push_back(cada_palabra);
     }
 
-    if (nombre_vector.size() != 3){
-        throw invalid_argument("Nombre incorrecto: debe tener un nombre y dos apellidos");
+    if (nombre_vector.size() != 3)
+    {
+      throw invalid_argument("Nombre incorrecto: debe tener un nombre y dos apellidos");
     }
 
     nombre = _nombre;
@@ -136,8 +149,9 @@ public:
 
   void set_edad(int _edad)
   {
-    if(_edad <= 18 || _edad >= 100 ){
-        throw out_of_range("Edad no valida, debe estar entre 18 a 100 ");
+    if (_edad <= 18 || _edad >= 100)
+    {
+      throw out_of_range("Edad no valida, debe estar entre 18 a 100 ");
     }
     edad = _edad;
   }
@@ -167,7 +181,7 @@ public:
   //-------------------------------------------------------------------------------------------
   // Lógica funcional
   //-------------------------------------------------------------------------------------------
-  
+
   // TODO: Método para abrir archivo txt estudiantes.txt
   // vector<Estudiante> o array, no sé que sea mejor, obtenerEstudiantes
   // En caso que no exista el archivo lanzar error
@@ -179,32 +193,43 @@ public:
   // Validaciones extras
   // Escribir los datos del estudiante en el txt y separar por ';' o '_' o '|', algún separador visible y poco usado
   //---------------------------------------------------------------------------------------------------------------------------------
-// Funciones para el manejo de archivo "estudiante.txt"
-//---------------------------------------------------------------------------------------------------------------------------------
-void guardar_estudiante_en_archivo(ofstream& archivo, const Estudiante& estudiante){
-  archivo << estudiante.get_id() <<";";
-  archivo << estudiante.get_nombre() <<";";
-  archivo << estudiante.get_edad() <<";";
-  archivo << estudiante.get_genero() <<";";
-  archivo << estudiante.get_provincia() <<",";
-  archivo << estudiante.get_canton() <<",";
-  archivo << estudiante.get_distrito() <<"/" <<endl;                  
-}
+  // Funciones para el manejo de archivo "estudiante.txt"
+  //---------------------------------------------------------------------------------------------------------------------------------
+  void guardar_estudiante_en_archivo(ofstream &archivo, const Estudiante &estudiante)
+  {
+    archivo << estudiante.get_id() << ";";
+    archivo << estudiante.get_nombre() << ";";
+    archivo << estudiante.get_edad() << ";";
+    archivo << estudiante.get_genero() << ";";
+    archivo << estudiante.get_provincia() << ",";
+    archivo << estudiante.get_canton() << ",";
+    archivo << estudiante.get_distrito() << "/" << endl;
+  }
 
-bool existe_id(const int id, ifstream& archivo){
-
-  string linea;
-    while (getline(archivo, linea)) {
-        stringstream ss(linea);
-        string identificacion;
-        if (getline(ss, identificacion, ';')) {
-          if (id == stoi(identificacion)){
-            return true;
-          }
+    Estudiante obtener_estudiante(const int id, ifstream &archivo)
+  {
+    string linea;
+    while (getline(archivo, linea))
+    {
+      stringstream ss(linea);
+      string identificacion;
+      int contador = 0;
+      string nombre; 
+      if (getline(ss, identificacion, ';'))
+      {
+        if (id == stoi(identificacion))
+        {
+          while(getline(ss, nombre, ';')){
+            contador ++; 
+            if (contador == 1){
+              return Estudiante(id,nombre);
+            }
+          } 
         }
+      }
     }
     archivo.close();
-    return false;
-}
+    return Estudiante();
+  }
 
 };
--- main.cpp
+++ main.cpp
@@ -2,18 +2,70 @@
 #include "estudiante.cpp"
 using namespace std;
 
-int main(){
-    Estudiante me(1234567891, "Me R H", 21, Genero::Femenino, "a", "b", "c");
-    cout<< "ID: " << me.get_id();
-    ofstream archivo("estudiantes.txt", ios::app);
-    if (archivo.is_open()) {
-        me.guardar_estudiante_en_archivo(archivo, me);
-        archivo.close();
-    }
-    ifstream estudiantes("estudiantes.txt", ios::app);
-    bool existe= me.existe_id(123589976, estudiantes);
-    cout<<"exist";
-    cout <<existe;
+// Funcion que solo imprime las opciones del menú.
+void imprimir_menu()
+{
+  cout << "----------------------------------------------------------------------------" << endl;
+  cout << "                        Menú Principal" << endl;
+  cout << "----------------------------------------------------------------------------" << endl;
+  cout << "1. Registrar Estudiante. " << endl;
+  cout << "2. Ingresar Calificaciones. " << endl;
+  cout << "3. Modificar datos Estudiante. " << endl;
+  cout << "4. Modificar Registro de Notas por Estudiante. " << endl;
+  cout << "5. Eliminar Registro de Estudiante " << endl;
+  cout << "6. Reporte de Estudiantes, promedios y estado" << endl;
+  cout << "7. Salir del Programa  " << endl;
+  cout << "Indique el número de la opción que desea realizar;" << endl;
+  cout << "---------------------------------------------------------------------------" << endl;
+}
 
-    return 0;
+int main()
+{
+  int opcion= 0;
+  do{
+    imprimir_menu();
+    cin >> opcion;
+    switch (opcion){
+      case 1:{
+
+      }
+
+      case 2:{
+
+      }
+
+      case 3:{
+        
+      }
+
+      case 4:{
+        
+      }
+
+      case 5:{
+        
+      }
+
+      case 6:{
+        
+      }
+
+    }
+  }
+  while( opcion != 7);
+  cout<<"Saliendo del programa";
+  Estudiante me(1234567891, "Me R H", 21, Genero::Femenino, "a", "b", "c");
+  cout << "ID: " << me.get_id();
+  ofstream archivo("estudiantes.txt", ios::app);
+  if (archivo.is_open())
+  {
+    me.guardar_estudiante_en_archivo(archivo, me);
+    archivo.close();
+  }
+  ifstream estudiantes("estudiantes.txt", ios::app);
+  Estudiante estudiante = estudiante.obtener_estudiante(123567891, estudiantes);
+  cout << "exist";
+  cout << estudiante.get_nombre();
+
+  return 0;
 }
